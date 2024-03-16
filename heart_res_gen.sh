1ee27086c490e4d0304867dc3d347965c4c52122
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-03-16 17:18:57 -0500

    Results

--- heart_res_gen.sh
+++ heart_res_gen.sh
@@ -1,29 +1,11 @@
-#python main.py --dataset   heart --activation sigmoid --optimizer adagrad --gradient batch --learning-rate 0.01 --epochs 50
-#python main.py --dataset   heart --activation sigmoid --optimizer momentum --gradient batch --learning-rate 0.01 --epochs 100
+#python main.py --dataset   heart --activation sigmoid --optimizer adagrad --gradient batch --learning-rate 0.001 --epochs 50
+#python main.py --dataset   heart --activation sigmoid --optimizer momentum --gradient batch --learning-rate 0.005 --epochs 100
 #python main.py --dataset   heart --activation sigmoid --optimizer none --gradient batch --learning-rate 0.05 --epochs 100
-#python main.py --dataset   heart --activation sigmoid --optimizer adagrad --gradient minibatch --learning-rate 0.00005 --epochs 500
-#python main.py --dataset   heart --activation sigmoid --optimizer momentum --gradient minibatch --learning-rate 0.01 --epochs 500
-#python main.py --dataset   heart --activation sigmoid --optimizer none --gradient minibatch --learning-rate 0.05 --epochs 500
-#python main.py --dataset   heart --activation sigmoid --optimizer adagrad --gradient stochastic --learning-rate 0.008 --epochs 800
-#python main.py --dataset   heart --activation sigmoid --optimizer momentum --gradient stochastic --learning-rate 0.05 --epochs 2000
-#python main.py --dataset   heart --activation sigmoid --optimizer none --gradient stochastic --learning-rate 0.1 --epochs 2500
 
-#python main.py --dataset   heart --activation tanh --optimizer adagrad --gradient batch --learning-rate 0.005 --epochs 100
-#python main.py --dataset   heart --activation tanh --optimizer momentum --gradient batch --learning-rate 0.01 --epochs 100
+#python main.py --dataset   heart --activation tanh --optimizer adagrad --gradient batch --learning-rate 0.0001 --epochs 100
+#python main.py --dataset   heart --activation tanh --optimizer momentum --gradient batch --learning-rate 0.005 --epochs 100
 #python main.py --dataset   heart --activation tanh --optimizer none --gradient batch --learning-rate 0.05 --epochs 100
-#python main.py --dataset   heart --activation tanh --optimizer adagrad --gradient minibatch --learning-rate 0.001 --epochs 200
-#python main.py --dataset   heart --activation tanh --optimizer momentum --gradient minibatch --learning-rate 0.008 --epochs 500
-#python main.py --dataset   heart --activation tanh --optimizer none --gradient minibatch --learning-rate 0.01 --epochs 500
-#python main.py --dataset   heart --activation tanh --optimizer adagrad --gradient stochastic --learning-rate 0.005 --epochs 500
-#python main.py --dataset   heart --activation tanh --optimizer momentum --gradient stochastic --learning-rate 0.01 --epochs 1000
-#python main.py --dataset   heart --activation tanh --optimizer none --gradient stochastic --learning-rate 0.05 --epochs 1500
 
-#python main.py --dataset   heart --activation relu --optimizer adagrad --gradient batch --learning-rate 0.0005 --epochs 50
+#python main.py --dataset   heart --activation relu --optimizer adagrad --gradient batch --learning-rate 0.00005 --epochs 50
 #python main.py --dataset   heart --activation relu --optimizer momentum --gradient batch --learning-rate 0.008 --epochs 100
 #python main.py --dataset   heart --activation relu --optimizer none --gradient batch --learning-rate 0.05 --epochs 100
-#python main.py --dataset   heart --activation relu --optimizer adagrad --gradient minibatch --learning-rate 0.0003 --epochs 100
-#python main.py --dataset   heart --activation relu --optimizer momentum --gradient minibatch --learning-rate 0.007 --epochs 500
-#python main.py --dataset   heart --activation relu --optimizer none --gradient minibatch --learning-rate 0.07 --epochs 500
-#python main.py --dataset   heart --activation relu --optimizer adagrad --gradient stochastic --learning-rate 0.0003 --epochs 500
-#python main.py --dataset   heart --activation relu --optimizer momentum --gradient stochastic --learning-rate 0.002 --epochs 1000
-#python main.py --dataset   heart --activation relu --optimizer none --gradient stochastic --learning-rate 0.1 --epochs 500
